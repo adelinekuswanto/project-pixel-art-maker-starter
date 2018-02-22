0fa53aa96c02f56cfb596b1608b9b29886dbea7d
Adeline-Kuswanto <adeline@tusitalabooks.com>
2018-02-22 20:59:10 +0800

    Added variables and event listener for submit button

--- designs.js
+++ designs.js
@@ -1,10 +1,29 @@
 // Select color input
 // Select size input
+$(function(){
+	
+	const height = $('#inputHeight').val();
+	const width = $('input#inputWidth').val();
+	const pixelCanvas = $('table');
+	const submitButton = $('input[type=submit]');
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
+		// When size is submitted by the user, call makeGrid()
+	submitButton.click(function makeGrid() {
+		for (var i = 0; i <= height; i++) {
+			pixelCanvas.append('<tr class="pixel-row"></tr>');
+		};
+		for (var m=0; i <= width; m++) {
+			$('.pixel-row').each(function() {
+				$(this).append('<td>Apple</td>');
+			});
+		};
+	});
 
-}
+
+
+
+
+
+});
