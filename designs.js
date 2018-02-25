41d91ab51155f3b562e6ee1a66bb6948a796cc36
Adeline Kuswanto <adeline@tusitalabooks.com>
2018-02-26 00:05:52 +0800

    Modified js file according to the style guide

--- designs.js
+++ designs.js
@@ -7,18 +7,18 @@ $('#sizePicker').submit(function makeGrid() {
   let row = $('input[name="height"]').val(),
   col = $('input[name="width"]').val();
   // Add new rows and columns to the grid
-	for (let i = 0; i < row; i++) {
+	for (let n = 0; n < row; n++) {
 		$('#pixelCanvas').append('<tr></tr>');
-    for (let j = 0; j < col; j++) {
+    for (let m = 0; m < col; m++) {
       $('#pixelCanvas tr').last().append('<td></td>');
-    };
-	};
+    }
+	}
   return false;
-});
+})
 
 // When color is picked by the user, call colorPick()
 $('#pixelCanvas').on('click', 'td', function colorPick() {
   // Select color input
   let colorPicker = $('input[type="color"]').val();
   $(this).css('background-color', colorPicker);
-});
+})
